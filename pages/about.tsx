52b88f86b3e7964cb9a6a7abb929ed74f4d1e16b
Steven Warner <stevenmichaelwarner@gmail.com>
2023-04-30 23:43:27 -0700

    add about page

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout';
+
+const About = () => {
+  return (
+    <Layout title="About">
+      <div className="flex flex-col items-center">
+        <h1 className="mb-4">About</h1>
+        <p>this is the about page</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
