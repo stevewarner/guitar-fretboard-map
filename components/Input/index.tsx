f5ebb2271b91b8589b10472d94dd1ff11e261def
Steven Warner <stevenmichaelwarner@gmail.com>
2024-12-05 20:46:51 -0800

    input error styling

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -27,13 +27,13 @@ export const Input = ({
     <>
       <label
         htmlFor={id}
-        className="block text-sm font-semibold leading-6 text-gray-900"
+        className={`block text-sm font-semibold leading-6 text-gray-900 ${error && 'text-red-600'}`}
       >
         {label}
       </label>
       <div className="mt-2.5">
         <input
-          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+          className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 ${error && 'ring-red-600'}`}
           id={id}
           ref={ref}
           value={value}
