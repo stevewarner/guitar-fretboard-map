508661f68a46696e0517d97e7ef26b3f864d8376
Steven Warner <stevenmichaelwarner@gmail.com>
2024-12-21 11:47:39 -0800

    icons update

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#4f46e5',
     icons: [
       {
-        src: '/images/icon-192.png',
+        src: '/web-app-manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
-        src: '/images/icon-512.png',
+        src: '/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
