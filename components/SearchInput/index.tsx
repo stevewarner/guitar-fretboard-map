ebc2108d1b1208813546b6bdf8f3bbb38c3993bf
Steve <stevenmichaelwarner@gmail.com>
2025-10-02 21:12:02 -0700

    fix: update searchinput to have clear button (#15)

--- components/SearchInput/index.tsx
+++ components/SearchInput/index.tsx
@@ -1,27 +1,46 @@
 import { InputHTMLAttributes } from 'react';
 import SearchIcon from '@/svgs/search.svg';
+import CloseIcon from '@/svgs/close.svg';
+
+interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  onClear?: () => void;
+}
 
 export const SearchInput = ({
-  value,
-  defaultValue,
+  id,
+  label = 'Search',
+  placeholder = 'Search',
+  value = '',
   onChange,
+  onClear,
   ...props
-}: InputHTMLAttributes<HTMLInputElement>) => {
-  return (
-    <div className="relative">
-      <SearchIcon
-        className="absolute left-4 top-1/2 translate-y-[-50%]"
-        height={20}
-        width={20}
-      />
-      <input
-        className="h-10 rounded border border-current pl-11 placeholder:text-current"
-        placeholder="Search"
-        value={value}
-        defaultValue={defaultValue}
-        onChange={onChange}
-        {...props}
-      />
-    </div>
-  );
-};
+}: SearchInputProps) => (
+  <div className="relative">
+    <SearchIcon
+      className="absolute left-4 top-1/2 translate-y-[-50%]"
+      height={20}
+      width={20}
+    />
+    <label htmlFor={id} className="sr-only">
+      {label}
+    </label>
+    <input
+      id={id}
+      className="h-10 rounded border border-current pl-11 pr-6 placeholder:text-current"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      {...props}
+    />
+    {value && onClear && (
+      <button
+        type="button"
+        className="absolute right-2 top-1/2 translate-y-[-50%] cursor-pointer hover:opacity-70"
+        onClick={() => onClear()}
+      >
+        <CloseIcon height={16} width={16} />
+      </button>
+    )}
+  </div>
+);
