5d892ba8e72b2a609ca2aa433f82af3780ab08c9
Steven Warner <stevenmichaelwarner@gmail.com>
2026-08-05 12:37:45 -0700

    fix: update robots

--- app/robots.ts
+++ app/robots.ts
@@ -3,11 +3,25 @@ import { SITE_URL } from '@/app/utils/site';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/print'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/print'],
+      },
+      {
+        userAgent: [
+          'meta-externalagent',
+          'GPTBot',
+          'ClaudeBot',
+          'CCBot',
+          'Google-Extended',
+          'Bytespider',
+          'Applebot-Extended',
+        ],
+        disallow: '/',
+      },
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
